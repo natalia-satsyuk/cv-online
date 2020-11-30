1999e35299dcf9d79a4150c25d2a558e240ad2c7
Nataliya <natalia.satsyuk@gmail.com>
2020-11-30 22:16:04 +0200

    add favicon, del trash

--- dist/js/script.js
+++ dist/js/script.js
@@ -52,40 +52,13 @@ function dropDown(){
 dropDown()
 
 // mobile-menu
-	let navButton = document.querySelector('.menu-icon');
-	let menu = document.querySelector('.navigation');	  
-  
-	navButton.onclick = function () { 
-		menu.classList.toggle('open');
-	}
+let navButton = document.querySelector('.menu-icon');
+let menu = document.querySelector('.navigation');	  
 
-// pop-up sertificates
-	// let showSeoSertificate = document.querySelector('#sertificate-seo-watch');
-	// let showTmSertificate = document.querySelector('#sertificate-tm-watch');
-	// let showNfSertificate = document.querySelector('#sertificate-nf-watch');
-	// let sertificate = document.querySelector('.education__sertificate');
+navButton.onclick = function () { 
+	menu.classList.toggle('open');
+}
 
-	// let sertificateSeo = document.querySelector('#sertificate-seo');
-	// let sertificateTm = document.querySelector('#sertificate-tm');
-	// let sertificateNf = document.querySelector('#sertificate-nf');
-
-	// let close = document.querySelector('.close');    
-	
-	// showSeoSertificate.onclick = function () { 
-	// 	sertificateSeo.classList.add('open');
-	// }
-
-	// showTmSertificate.onclick = function () { 
-	// 	sertificateTm.classList.add('open');
-	// }
-
-	// showNfSertificate.onclick = function () { 
-	// 	sertificateNf.classList.add('open');
-	// }
-  
-	// close.onclick = function () {
-	//   sertificate.classList.remove('open');
-	// } 
 const navlink = document.querySelectorAll(".menu__link");
 
 navlink.forEach(elem => elem.addEventListener("click", smoothscroll));
